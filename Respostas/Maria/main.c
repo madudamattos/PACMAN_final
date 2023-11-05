8dbd9f549f2ccd80d020e3f645d57462c81967d6
madudamattos <mariaeduardanmattos@gmail.com>
2023-11-05 14:07:35 -0300

    Cria fantasma funcionando corretamente

--- Respostas/Maria/main.c
+++ Respostas/Maria/main.c
@@ -7,7 +7,10 @@
 #include "tTunel.h"
 #include "tMovimento.h"
 #include "tPacman.h"
+#include "tArquivo.h"
+//#include "tFantasma.h"
 
+//funcoes t Fantasma--------------
 typedef struct{
     char tipo;
     COMANDO movimento;
@@ -15,27 +18,44 @@ typedef struct{
     tPosicao *posicao;
 } tFantasma;
 
-
-//funcoes de realizar jogo
 tFantasma** CriaFantasmas(tMapa *mapa);
 void MoveFantasmas(tMapa *mapa, tFantasma **fantasmas);
+bool EncontrouFantasma(tFantasma **fantasmas, tPosicao *posicao);
+void DesalocaFantasmas(tFantasma **fantasmas);
+COMANDO InverteMovimento(COMANDO comando);
 
-//funcoes 
+//funcoes tArquivo-------------------------------
 void ImprimeMapa(tMapa *mapa, tPacman *pacman, FILE *pArquivo);
-
-//funcoes de geracao de arquivo
 void GeraArquivoInicializacao(tMapa *mapa, tPacman *pacman);
 void GeraArquivoEstatisticas(tPacman *pacman);
 void GeraArquivoResumo(tPacman *pacman);
 void AtualizaArquivoSaida(tMapa *mapa, tPacman *pacman, FILE *pFile);
 void FinalizaArquivoSaida(tPacman *pacman, FILE *pSaida);
 FILE* GeraArquivoSaida();
-
-//funcoes auxiliares
-COMANDO InverteMovimento(COMANDO comando);
 int PrioridadeDoMovimento(COMANDO comando);
 char RetornaTeclaEquivalente(COMANDO comando);
 
+// typedef struct{
+//     tMapa *mapa;
+//     tPacman	*pacman;
+//     tFantasma **fantasmas;
+//     tSimbolos identificador;
+//     int gameOver; 
+// }tJogo;
+
+// typedef struct{
+//     char comida;
+//     char pacman;
+//     char fantasmaP;
+//     char fantasmaB;
+//     char fantasmaC;
+//     char fantasmaI;
+//     char portal;
+//     char parede;
+//     char vazio;
+// }tSimbolos;
+
+//funcoes de jogo
 
 int main(int argc, char *argv[]){
     char *caminhoConfig = argv[1];
@@ -49,14 +69,20 @@ int main(int argc, char *argv[]){
 
     sprintf(caminhoPastaSaida, "%s/saida/", caminhoConfig);
 
+    //definicoes iniciais
     tMapa *mapa = CriaMapa(caminhoConfig);
-
     tPacman *pacman = CriaPacman(ObtemPosicaoItemMapa(mapa, '>'));
- 
-    
-    //arquivo saida
-    GeraArquivoInicializacao(mapa, pacman);
+    tFantasma **fantasmas = CriaFantasmas(mapa);
 
+    //teste printa fantasma
+
+    // printf("%c\n", fantasmas[0]->tipo);
+    // printf("%d\n", fantasmas[0]->status);
+    // printf("%d\n", fantasmas[0]->movimento);
+    // printf("%d %d\n", fantasmas[0]->posicao->linha,fantasmas[0]->posicao->coluna);
+
+    //arquivo inicializacao e saida
+    GeraArquivoInicializacao(mapa, pacman);
     pSaida = GeraArquivoSaida(); 
     AtualizaArquivoSaida(mapa, pacman, pSaida);
 
@@ -70,77 +96,34 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-void ImprimeMapa(tMapa *mapa, tPacman *pacman, FILE *pArquivo){
-    int L = ObtemNumeroLinhasMapa(mapa);
-    int C = ObtemNumeroColunasMapa(mapa);
-    tPosicao *posicao = NULL;
-    tPosicao *posicaoPacman = ClonaPosicao(ObtemPosicaoPacman(pacman));
-    int i, j;
-
-    for(i=0; i<L;i++){
-        for(j=0; j<C;j++){
-            posicao = CriaPosicao(i, j);
-            if(SaoIguaisPosicao(posicao, posicaoPacman)){
-                fprintf(pArquivo, ">");
-            }
-            else{
-                fprintf(pArquivo, "%c", ObtemItemMapa(mapa, posicao));
-            }
-            DesalocaPosicao(posicao);
-        }
-        fprintf(pArquivo, "\n");
-    }
-
-    DesalocaPosicao(posicaoPacman);
-}
-
-//funcoes jogo
+//funcoes tFantasma--------------------------------
 
 tFantasma** CriaFantasmas(tMapa *mapa){
-    tFantasma** fantasmas = calloc(4, sizeof(tFantasma));
-
-    //cria B
-    fantasmas[0]->tipo = 'B';
-    fantasmas[0]->movimento = MOV_ESQUERDA;
-    fantasmas[0]->posicao = ObtemPosicaoItemMapa(mapa, fantasmas[0]->tipo);
-    if(ObtemPosicaoItemMapa(mapa, 'B') == NULL){
-        fantasmas[0]->status = 0;
-    }
-    else{
-        fantasmas[0]->status = 1;
+    tFantasma** fantasmas = calloc(4, sizeof(tFantasma*));
+    if(fantasmas == NULL){
+        printf("Erro na alocacao do vetor de fantasmas\n");
+        exit(1);
     }
 
-    //cria P
-    fantasmas[1]->tipo = 'P';
-    fantasmas[1]->movimento = MOV_CIMA;
-    fantasmas[1]->posicao = ObtemPosicaoItemMapa(mapa, fantasmas[1]->tipo);
-    if(ObtemPosicaoItemMapa(mapa, fantasmas[1]->tipo) == NULL){
-        fantasmas[1]->status = 0;
-    }
-    else{
-        fantasmas[1]->status = 1;
-    }
+    int i;
 
-    //cria I
-    fantasmas[2]->tipo = 'I';
-    fantasmas[2]->movimento = MOV_BAIXO;
-    fantasmas[2]->posicao = ObtemPosicaoItemMapa(mapa, fantasmas[2]->tipo);
-    if(ObtemPosicaoItemMapa(mapa, fantasmas[2]->tipo) == NULL){
-        fantasmas[2]->status = 0;
-    }
-    else{
-        fantasmas[2]->status = 1;
-    }
+    for(i=0;i<4;i++){
+        fantasmas[i] = calloc(1, sizeof(tFantasma));
+        fantasmas[i]->movimento = i;
 
-    //cria C
-    fantasmas[3]->tipo = 'C';
-    fantasmas[3]->movimento = MOV_DIREITA;
-    fantasmas[3]->posicao = ObtemPosicaoItemMapa(mapa, fantasmas[3]->tipo);
-    if(ObtemPosicaoItemMapa(mapa, fantasmas[3]->tipo) == NULL){
-        fantasmas[3]->status = 0;
-    }
-    else{
-        fantasmas[3]->status = 1;
+        if(i == 0) fantasmas[i]->tipo = 'B';
+        else if (i == 1) fantasmas[i]->tipo = 'P';
+        else if (i == 2) fantasmas[i]->tipo = 'I';
+        else fantasmas[i]->tipo = 'C';
+
+        fantasmas[i]->posicao = ObtemPosicaoItemMapa(mapa, fantasmas[i]->tipo);
+        if(fantasmas[i]->posicao != NULL){
+            fantasmas[i]->status = 1;
+            AtualizaItemMapa(mapa,fantasmas[i]->posicao, ' ');
+        }
+        else{
+            fantasmas[i]->status = 0;
+        }
     }
 
     return fantasmas;
@@ -214,7 +197,62 @@ void DesalocaFantasmas(tFantasma **fantasmas){
     free(fantasmas);
 }
 
-//funcoes de geracao de arquivo
+bool EncontrouFantasma(tFantasma **fantasmas, tPosicao *posicao){
+    int i;
+
+    for(i=0; i<4; i++){
+        if(SaoIguaisPosicao(fantasmas[i]->posicao, posicao)){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+COMANDO InverteMovimento(COMANDO comando){
+    COMANDO movimentoOposto;
+    
+    if(comando == 1){
+        movimentoOposto = 2;
+    }
+    else if(comando == 2){
+        movimentoOposto = 1;
+    }
+    else if(comando == 3){
+        movimentoOposto = 0;
+    }
+    else if(comando == 0){
+        movimentoOposto = 3;
+    }
+    
+    return movimentoOposto;
+}
+
+//funcoes tArquivo -------------------------------------
+
+void ImprimeMapa(tMapa *mapa, tPacman *pacman, FILE *pArquivo){
+    int L = ObtemNumeroLinhasMapa(mapa);
+    int C = ObtemNumeroColunasMapa(mapa);
+    tPosicao *posicao = NULL;
+    tPosicao *posicaoPacman = ClonaPosicao(ObtemPosicaoPacman(pacman));
+    int i, j;
+
+    for(i=0; i<L;i++){
+        for(j=0; j<C;j++){
+            posicao = CriaPosicao(i, j);
+            if(SaoIguaisPosicao(posicao, posicaoPacman)){
+                fprintf(pArquivo, ">");
+            }
+            else{
+                fprintf(pArquivo, "%c", ObtemItemMapa(mapa, posicao));
+            }
+            DesalocaPosicao(posicao);
+        }
+        fprintf(pArquivo, "\n");
+    }
+
+    DesalocaPosicao(posicaoPacman);
+}
 
 void GeraArquivoInicializacao(tMapa *mapa, tPacman *pacman){
     char arquivoInicializacao[100];
@@ -222,10 +260,8 @@ void GeraArquivoInicializacao(tMapa *mapa, tPacman *pacman){
     
     sprintf(arquivoInicializacao, "inicializacao.txt");
 
-    printf("6");
     pInit = fopen(arquivoInicializacao, "w");
 
-    printf("7");
     ImprimeMapa(mapa, pacman, pInit);
 
     tPosicao* posicaoPacman = ObtemPosicaoPacman(pacman);
@@ -318,6 +354,7 @@ void AtualizaArquivoSaida(tMapa *mapa, tPacman *pacman, FILE *pFile){
 }
 
 void FinalizaArquivoSaida(tPacman *pacman, FILE *pSaida){
+
     if(1){
         fprintf(pSaida, "Voce venceu!\n");
     }
@@ -330,27 +367,6 @@ void FinalizaArquivoSaida(tPacman *pacman, FILE *pSaida){
     fprintf(pSaida,"Pontuacao final: %d\n", pontuacaoFinal);
 
     fclose(pSaida);
-}
-
-//funcoes auxiliares ------------------------------------------------
-
-COMANDO InverteMovimento(COMANDO comando){
-    COMANDO movimentoOposto;
-    
-    if(comando == 1){
-        movimentoOposto = 2;
-    }
-    else if(comando == 2){
-        movimentoOposto = 1;
-    }
-    else if(comando == 3){
-        movimentoOposto = 0;
-    }
-    else if(comando == 0){
-        movimentoOposto = 3;
-    }
-    
-    return movimentoOposto;
 }
 
 int PrioridadeDoMovimento(COMANDO comando){
@@ -394,3 +410,5 @@ char RetornaTeclaEquivalente(COMANDO comando){
 
     return tecla;
 }
+
+//t----------------------------------------
